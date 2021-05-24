8c830b89a34ca111b41a9e1ca45c753624759093
Mr.Sourav Ghosh <37017415+ghsourav@users.noreply.github.com>
2021-05-24 22:24:59 +0530

    Update Date

--- js/Apps.js
+++ js/Apps.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
   let output = ""
   var pincode = ""
-  $('.datepicker').datepicker({ "format": 'dd-mm-yyyy', "showDaysInNextAndPreviousMonths":'true' });
+  $('.datepicker').datepicker({ "format": 'dd-mm-yyyy', "showDaysInNextAndPreviousMonths":'false' });
 
   $('.tooltipped').tooltip();
 
